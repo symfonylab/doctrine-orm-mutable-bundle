0e4cbd31e2e8e2badd8ce16f58c26978154366fd
Ihor Melnichenko <igor@creawill.com>
2019-07-30 15:59:20 +0300

    first commit

---
+++ DoctrineOrmMutableBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SymfonyLab\DoctrineOrmMutable;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoctrineOrmMutableBundle extends Bundle
+{
+
+}
